8d5c8475b426314b5c9897166b5f55c29e88c8ee
Christian <cwaters@truenorthit.co.uk>
2023-12-05 22:29:11 +0000

    more fiddling

--- 2023_05/Program.cs
+++ 2023_05/Program.cs
@@ -17,23 +17,18 @@ foreach (var line in lines.Skip(1).Where(str => !String.IsNullOrEmpty(str)))
     maps[current].Add(new (sp[0], sp[1], sp[2]));
 }
 
-var part1 = seeds
-    .Select(seed => solve(maps, new List<Range>() { new (seed, seed + 1) }))
-    .Min();
-
+var part1 = seeds.Select(seed => solve(maps, new List<Range>() { new (seed, seed + 1) })).Min();
 Console.WriteLine($"Part1: {part1} in {stopwatch.ElapsedMilliseconds}ms");
 
 var part2 = seeds
     .Select((seed, i) => (seed, i))
-    .GroupBy(tp => tp.i / 2)
-    .Select(grp => (grp.First().seed, grp.Skip(1).First().seed))
-    .Select(pair => solve(maps, new List<Range>() { new (pair.Item1, pair.Item1 + pair.Item2) }))
+    .GroupBy(tp => tp.i / 2, (key, grp) => solve(maps, 
+        new Range[] { new (grp.First().seed, grp.First().seed + grp.Skip(1).First().seed) }))
     .Min();
-
 Console.WriteLine($"Part2: {part2} in {stopwatch.ElapsedMilliseconds}ms");
 
-static long solve(List<Map>[] maps, List<Range> source) => maps.Aggregate(source, (acc, m) => map(m, acc), acc => acc.Min(tp => tp.start));
-static List<Range> map(List<Map> maps, List<Range> source)
+static long solve(List<Map>[] maps, IEnumerable<Range> source) => maps.Aggregate(source, (acc, m) => map(m, acc), acc => acc.Min(tp => tp.start));
+static List<Range> map(List<Map> maps, IEnumerable<Range> source)
 {
     var destRanges = new List<Range>();
     foreach (var (rangeStart, rangeEnd) in source)
@@ -45,21 +40,19 @@ static List<Range> map(List<Map> maps, List<Range> source)
 
             // R    |-----|
             // M |----|
-
-            var mappedMiddle = new Range(Math.Max(rangeStart, mapSource), Math.Min(rangeEnd, mapSource + mapLength));
-
-            if (mappedMiddle.start < mappedMiddle.end)
+            var mapped = new Range(Math.Max(rangeStart, mapSource), Math.Min(rangeEnd, mapSource + mapLength));
+            if (mapped.start < mapped.end)
             {
-                //this map intersected this range so add the mapped results
-                destRanges.AddRange(new List<Range>() { new(mapDestination + mappedMiddle.start - mapSource, mapDestination + mappedMiddle.end - mapSource) });
-                //and recurse over any unmapped sections
-                //if overlaps, add result, find remaining ranges and recursively call
+                //this map intersected this range so add the mapped section as a result
+                destRanges.AddRange(new Range[] { new(mapDestination + mapped.start - mapSource, mapDestination + mapped.end - mapSource) });
+                //and for any unmapped splits, recurse and add their results
+                //*** seems that none of these splits actually contribute to the result for my input ***
                 destRanges.AddRange(
                     new Range[] { 
-                        new(rangeStart, Math.Max(rangeStart, mapSource)), 
-                        new(Math.Max(rangeEnd, mapSource + mapLength), rangeEnd) }
+                        new(rangeStart, Math.Min(rangeEnd, mapSource)), 
+                        new(Math.Max(rangeStart, mapSource + mapLength), rangeEnd) }
                     .Where(range => range.start < range.end)
-                    .SelectMany(range => map(maps, new List<Range>() { range })));
+                    .SelectMany(range => map(maps, new Range[] { range })));
                 goto mapped;
             }
         }
