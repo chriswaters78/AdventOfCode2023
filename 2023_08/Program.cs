281ecbae7335b0ac7ac07dd3b2ca26be340b49c6
Christian <cwaters@truenorthit.co.uk>
2023-12-08 08:51:00 +0000

    day 8

---
+++ 2023_08/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace _2023_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = File.ReadAllLines("input.txt");
+            var instructions = file[0];
+            var map = file.Skip(2).ToDictionary(line => line.Split(" = ")[0], line => line.Split(" = ")[1].Trim('(', ')').Split(", ").ToList());
+
+            //{
+            //    var current = "AAA";
+            //    int steps = 0;
+            //    for (var i = 0; i < int.MaxValue; i++)
+            //    {
+            //        steps++;
+            //        current = map[current][instructions[i % instructions.Length] == 'L' ? 0 : 1];
+            //        if (current == "ZZZ")
+            //        {
+            //            break;
+            //        }
+            //    }
+            //    Console.WriteLine($"Part1: {steps}");
+            //}
+
+            //do some maths!
+            long l = 71958382637 * 293;
+            Console.WriteLine($"{l}");
+            {
+                var currents = map.Keys.Where(key => key.EndsWith('A')).ToList();
+                var foundEnd = new HashSet<string>();
+                Console.WriteLine($"{currents.Count} starting nodes");
+                var steps = 0;
+                for (var i = 0; i < int.MaxValue; i++)
+                {
+                    steps++;
+                    var nextCurrents = new List<string>();
+                    foreach (var current in currents)
+                    {
+                        var next = map[current][instructions[i % instructions.Length] == 'L' ? 0 : 1];
+                        if (next.EndsWith('Z'))
+                        {
+                            Console.WriteLine($"Found end {next} in {steps}: factor {steps / 293}");
+                            foundEnd.Add(next);
+                        }
+                        nextCurrents.Add(next);
+                    }
+                    if (nextCurrents.All(key => key.EndsWith('Z')))
+                    {
+                        break;
+                    }
+                    currents = nextCurrents;
+                }
+                Console.WriteLine($"Part2: {steps}");
+            }
+
+        }
+    }
+}
