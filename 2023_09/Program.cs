d03fa9596343a05d7e2d509112a87abc2cbbf7d4
Christian Waters <cwaters@truenorthit.co.uk>
2023-12-09 17:26:05 +0000

    .

--- 2023_09/Program.cs
+++ 2023_09/Program.cs
@@ -1,38 +1,28 @@
-﻿namespace _2023_09
+﻿int part1 = 0;
+int part2 = 0;
+foreach (var line in File.ReadAllLines("input.txt"))
 {
-    internal class Program
+    var sequences = new List<List<int>>();
+    sequences.Add(line.Split(" ").Select(int.Parse).ToList());
+    while (sequences.Last().Any(v => v != 0))
     {
-        static void Main(string[] args)
+        var newSequence = new List<int>();
+        for (int i = 1; i < sequences.Last().Count; i++)
         {
-            int part1 = 0;
-            int part2 = 0;
-            foreach (var line in File.ReadAllLines("input.txt"))
-            {
-                var sequences = new List<List<int>>();
-                sequences.Add(line.Split(" ").Select(int.Parse).ToList());
-                while (sequences.Last().Any(v => v != 0))
-                {
-                    var newSequence = new List<int>();
-                    for (int i = 1; i < sequences.Last().Count; i++)
-                    {
-                        newSequence.Add(sequences.Last()[i] - sequences.Last()[i - 1]);
-                    }
-                    sequences.Add(newSequence);
-                }
-                //add new zero
-                sequences.Last().Add(0);
-                sequences.Last().Insert(0, 0);
-                for (int s = sequences.Count - 2; s >= 0; s--)
-                {
-                    sequences[s].Add(sequences[s].Last() + sequences[s + 1].Last());
-                    sequences[s].Insert(0, sequences[s].First() - sequences[s + 1].First());
-                }
-                part1 += sequences[0].Last();
-                part2 += sequences[0].First();
-            }
-
-            Console.WriteLine($"Part1: {part1}");
-            Console.WriteLine($"Part2: {part2}");
+            newSequence.Add(sequences.Last()[i] - sequences.Last()[i - 1]);
         }
+        sequences.Add(newSequence);
     }
+    sequences.Last().Add(0);
+    sequences.Last().Insert(0, 0);
+    for (int s = sequences.Count - 2; s >= 0; s--)
+    {
+        sequences[s].Add(sequences[s].Last() + sequences[s + 1].Last());
+        sequences[s].Insert(0, sequences[s].First() - sequences[s + 1].First());
+    }
+    part1 += sequences[0].Last();
+    part2 += sequences[0].First();
 }
+
+Console.WriteLine($"Part1: {part1}");
+Console.WriteLine($"Part2: {part2}");
