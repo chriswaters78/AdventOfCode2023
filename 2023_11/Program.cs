05cc830df296c0d54859404e255e61751da3b4c6
Christian Waters <cwaters@truenorthit.co.uk>
2023-12-11 19:36:57 +0000

    f#, seems slow?

--- 2023_11/Program.cs
+++ 2023_11/Program.cs
@@ -1,5 +1,7 @@
 ﻿const long EXPANSION = 1000000;
 var file = File.ReadAllLines("input.txt");
+var watch = new System.Diagnostics.Stopwatch();
+watch.Start();
 
 var points = Enumerable.Range(0, file.Length).SelectMany(y => Enumerable.Range(0, file.First().Length).Select(x => new Point(x, y)).Where(p => file[p.y][p.x] == '#')).ToList();
 var fullRows = new HashSet<int>(points.Select(p => p.y).Distinct());
@@ -9,5 +11,5 @@ var answer = Enumerable.Range(0, points.Count).Sum(p1 => Enumerable.Range(p1 + 1
     Enumerable.Range(Math.Min(points[p1].x, points[p2].x), Math.Abs(points[p1].x - points[p2].x)).Sum(x => !fullColumns.Contains(x) ? EXPANSION : 1L)
     + Enumerable.Range(Math.Min(points[p1].y, points[p2].y), Math.Abs(points[p1].y - points[p2].y)).Sum(y => !fullRows.Contains(y) ? EXPANSION : 1L)));
 
-Console.WriteLine($"Distance {answer} for expansion {EXPANSION}");
+Console.WriteLine($"Distance {answer} for expansion {EXPANSION} in {watch.ElapsedMilliseconds}ms");
 record struct Point(int x, int y);
