3448d75c053e0c17423ae32225038690c7e43b2b
Christian Waters <cwaters@truenorthit.co.uk>
2023-12-12 15:18:10 +0000

    refactored

--- 2023_12/Program.cs
+++ 2023_12/Program.cs
@@ -4,52 +4,43 @@ namespace _2023_12
 {
     internal class Program
     {
-        static Dictionary<string, BigInteger> stateCache = new Dictionary<string, BigInteger>();
+        static Dictionary<string, long> stateCache = new Dictionary<string, long>();
         static void Main(string[] args)
         {
             var masks = new List<string>();
             var specs = new List<List<int>>();
-            foreach (var line in File.ReadAllLines("input.txt"))
+            foreach (var line in File.ReadAllLines("test.txt"))
             {
                 var sp = line.Split(" ");
                 masks.Add(sp[0]);
                 specs.Add(sp[1].Split(",").Select(int.Parse).ToList());
             }
 
-            BigInteger part1 = 0;
-            for (int i = 0; i < masks.Count; i++)
-            {
-                var initialState = new State("", masks[i], 0, specs[i]);
-                part1 += consume(initialState);
-            }
+            Func<long> solve = () => masks.Zip(specs).Select(a => consume(new State(a.First, 0, a.Second))).Sum();
+            long part1 = solve();
             Console.WriteLine($"Part1: {part1}");
 
-            var masks2 = masks.ToList();
-            var specs2 = specs.Select(list => list.ToList()).ToList();
-            for (int count = 0; count < 4; count++)
+            for (int i = 0; i < masks.Count; i++)
             {
-                for (int i = 0; i < masks2.Count; i++)
-                {
-                    masks2[i] = $"{masks2[i]}?{masks[i]}";
-                    specs2[i].AddRange(specs[i]);
-                }
+                masks[i] = String.Join("?",Enumerable.Repeat(masks[i], 5));
+                specs[i] = Enumerable.Repeat(0,5).SelectMany(_ => specs[i]).ToList();
             }
-            BigInteger part2 = 0;
-            for (int i = 0; i < masks2.Count; i++)
-            {
-                var initialState = new State("", masks2[i], 0, specs2[i]);
-                var ans = consume(initialState);
-                part2 += ans;
-            }
+            long part2 = solve();
             Console.WriteLine($"Part2: {part2}");
-
         }
 
-        static BigInteger consume(State state)
+        static long consume(State state)
         {
+            var cacheKey = stateToCacheKey(state);
+            if (stateCache.ContainsKey(cacheKey))
+            {
+                return stateCache[cacheKey];
+            }
+
             if (state.remainingMask.Length == 0)
             {
-                if ((state.remainingSpec.Count == 0 && state.noBroken == 0) || (state.remainingSpec.Count == 1 && state.remainingSpec[0] == state.noBroken))
+                if (    (state.remainingSpec.Count == 0 && state.noBroken == 0) 
+                    ||  (state.remainingSpec.Count == 1 && state.remainingSpec[0] == state.noBroken))
                 {
                     return 1;
                 }
@@ -60,7 +51,7 @@ namespace _2023_12
                 ? new char[] { '.', '#' }
                 : new char[] { state.remainingMask[0] };
 
-            BigInteger total = 0;
+            long total = 0;
             foreach (var mask in currMask)
             {
                 switch (mask)
@@ -68,23 +59,11 @@ namespace _2023_12
                     case '.':
                         if (state.noBroken == 0)
                         {
-                            var newState = new State($"{state.consumed}.", state.remainingMask[1..], state.noBroken, state.remainingSpec);
-                            var stateKey = stateToCacheKey(newState);
-                            if (!stateCache.ContainsKey(stateKey))
-                            {
-                                stateCache[stateKey] = consume(newState);
-                            }
-                            total += stateCache[stateKey];
+                            total += consume(new State(state.remainingMask[1..], state.noBroken, state.remainingSpec));
                         }
                         else if (state.remainingSpec.Any() && state.noBroken == state.remainingSpec.First())
                         {
-                            var newState = new State($"{state.consumed}.", state.remainingMask[1..], 0, state.remainingSpec.Skip(1).ToList());
-                            var stateKey = stateToCacheKey(newState);
-                            if (!stateCache.ContainsKey(stateKey))
-                            {
-                                stateCache[stateKey] = consume(newState);
-                            }
-                            total += stateCache[stateKey];
+                            total += consume(new State(state.remainingMask[1..], 0, state.remainingSpec.Skip(1).ToList()));
                         }
                         break;
                     case '#':
@@ -94,18 +73,13 @@ namespace _2023_12
                         }
                         else
                         {
-                            var newState = new State($"{state.consumed}#", state.remainingMask[1..], state.noBroken + 1, state.remainingSpec);
-                            var stateKey = stateToCacheKey(newState);
-                            if (!stateCache.ContainsKey(stateKey))
-                            {
-                                stateCache[stateKey] = consume(newState);
-                            }
-                            total += stateCache[stateKey];
+                            total += consume(new State(state.remainingMask[1..], state.noBroken + 1, state.remainingSpec));
                         }
                         break;
                 }
             }
 
+            stateCache[cacheKey] = total;
             return total;
         }
 
@@ -113,6 +87,6 @@ namespace _2023_12
         {
             return $"{state.remainingMask}:{state.noBroken}:{String.Join(",", state.remainingSpec)}";
         }
-        record struct State(string consumed, string remainingMask, int noBroken, List<int> remainingSpec);
+        record struct State(string remainingMask, int noBroken, List<int> remainingSpec);
     }
 }
