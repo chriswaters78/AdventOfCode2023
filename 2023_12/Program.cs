87081cd3273d7515bfec8d2e19c382167ffa6e8a
Christian Waters <cwaters@truenorthit.co.uk>
2023-12-13 21:47:21 +0000

    ~ 4 x faster without LINQ inner loop

--- 2023_12/Program.cs
+++ 2023_12/Program.cs
@@ -1,51 +1,43 @@
-﻿namespace _2023_12
+﻿System.Diagnostics.Stopwatch watch = new System.Diagnostics.Stopwatch();
+watch.Start();
+var rows = File.ReadAllLines("input.txt").Select(line => line.Split(" "))
+    .Select(sp => new Row( $"{sp[0]}", sp[1].Split(",").Select(int.Parse).ToList())).ToList();
+
+var part1 = solve(rows);
+Console.WriteLine($"Part1: {part1} in {watch.ElapsedMilliseconds}ms");
+
+var part2 = solve(
+    rows.Select(puzzle => new Row(
+        $"{String.Join("?", Enumerable.Repeat(puzzle.condition, 5))}",
+        Enumerable.Repeat(0, 5).SelectMany(_ => puzzle.spec).ToList()
+)));
+
+Console.WriteLine($"Part2: {part2} in {watch.ElapsedMilliseconds}ms");
+
+long solve(IEnumerable<Row> rows) => rows.AsParallel().Select(row => consume(new State(0, 0, row.spec.Count), new(), $"{row.condition}.", row.spec)).Sum();
+long consume(State state, Dictionary<State, long> stateCache, string mask, List<int> spec)
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            var puzzles = File.ReadAllLines("input.txt").Select(line => line.Split(" "))
-                .Select(sp => new { masks = sp[0], specs = sp[1].Split(",").Select(int.Parse).ToList() }).ToList();
-
-            Func<long> solve = () => puzzles.Select(a => consume(new State($"{a.masks}.", 0, a.specs), new())).Sum();
-            Console.WriteLine($"Part1: {solve()}");
-
-            puzzles = puzzles.Select(puzzle => new {
-                masks = String.Join("?", Enumerable.Repeat(puzzle.masks, 5)),
-                specs = Enumerable.Repeat(0, 5).SelectMany(_ => puzzle.specs).ToList()
-            }).ToList();
-
-            Console.WriteLine($"Part2: {solve()}");
-        }
-
-        static long consume(State state, Dictionary<string,long> stateCache)
-        {
-            var cacheKey = stateToCacheKey(state);
-            if (stateCache.ContainsKey(cacheKey))
-                return stateCache[cacheKey];
+    if (stateCache.ContainsKey(state))
+        return stateCache[state];
             
-            if (state.remainingMask.Length == 0)
-                return state.remainingSpec.Any() ? 0 : 1;
+    if (state.consumed == mask.Length)
+        return state.remainingRuns == 0 ? 1 : 0;
 
-            var currMask = state.remainingMask[0] == '?' ? ".#" : state.remainingMask[0..1];
-            stateCache[cacheKey] = currMask.Sum( c => c switch
-                {
-                    '.' when state.currentRun == 0
-                        => consume(new State(state.remainingMask[1..], 0, state.remainingSpec), stateCache),
-                    '.' when state.currentRun == state.remainingSpec.FirstOrDefault()
-                        => consume(new State(state.remainingMask[1..], 0, state.remainingSpec.Skip(1).ToList()), stateCache),
-                    '.' => 0,
-                    '#' => consume(new State(state.remainingMask[1..], state.currentRun + 1, state.remainingSpec), stateCache)
-                });
+    var charCanBe = mask[state.consumed] == '?' ? ".#" : $"{mask[state.consumed]}";
+    var result = 0L;
+    foreach (var ch in charCanBe)
+        result += ch switch
+        {
+            '.' when state.run == 0
+                => consume(new State(state.consumed + 1, 0, state.remainingRuns), stateCache, mask, spec),
+            '.' when state.remainingRuns >= 1 && state.run == spec[spec.Count - state.remainingRuns]
+                => consume(new State(state.consumed + 1, 0, state.remainingRuns - 1), stateCache, mask, spec),
+            '.' => 0,
+            '#' => consume(new State(state.consumed + 1, state.run + 1, state.remainingRuns), stateCache, mask, spec)
+        };
 
-            return stateCache[cacheKey];
-        }
-
-        //max length of mask ~100 and spec ~ 30
-        //max length of run is same as mask
-        //so max number of cache entries is 100^2 * 30 = 300,000
-        private static string stateToCacheKey(State state) 
-            => $"{state.remainingMask.Length}:{state.currentRun}:{state.remainingSpec.Count}";
-        record struct State(string remainingMask, int currentRun, List<int> remainingSpec);
-    }
+    return stateCache[state] = result;
 }
+
+record struct State(int consumed, int run, int remainingRuns);
+record struct Row(string condition, List<int> spec);
