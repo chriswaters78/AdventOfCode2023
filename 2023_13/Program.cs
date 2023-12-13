508556703a4de4eac667bf3d1420cb2c94b3f949
Christian Waters <cwaters@truenorthit.co.uk>
2023-12-13 22:56:58 +0000

    faster method

--- 2023_13/Program.cs
+++ 2023_13/Program.cs
@@ -1,97 +1,31 @@
 ﻿System.Diagnostics.Stopwatch watch = new System.Diagnostics.Stopwatch();
 watch.Start();
 
-var lines = File.ReadAllLines("input.txt");
-var byRow = new List<List<char[]>>();
-var current = new List<char[]>();
-foreach (var line in lines)
+var byRow = File.ReadAllText("input.txt").Split($"{Environment.NewLine}{Environment.NewLine}")
+    .Select(grid => grid.Split($"{Environment.NewLine}")).ToArray();
+
+var byColumn = byRow.Select(grid =>
+    Enumerable.Range(0, grid[0].Length).Select(c => String.Join("", 
+            Enumerable.Range(0, grid.Length).Select(r => grid[r][c]))).ToArray()).ToArray();
+
+Func<List<(int, int)>, int, List<int>> filter = (reflections, smudges) => reflections.Where(tp => tp.Item2 == smudges).Select(tp => tp.Item1).ToList();
+
+(var part1, var part2) = byColumn.Zip(byRow).Select(tp =>
 {
-    if (String.IsNullOrEmpty(line))
-    {
-        byRow.Add(current);
-        current = new List<char[]>();
-        continue;
-    }
-    current.Add(line.ToArray());
-}
-byRow.Add(current);
+    (var hR, var vR) = (findReflectionSmudges(tp.Second), findReflectionSmudges(tp.First));    
+    (var hR0, var vR0) = (filter(hR,0), filter(vR, 0));
+    (var hR1, var vR1) = (filter(hR, 1).Except(hR0), filter(vR, 1).Except(vR0));
 
-var byColumn = new List<List<char[]>>();
-foreach (var gridR in byRow)
-{
-    var gridC = new List<char[]>();
-    for (int c = 0; c < gridR.First().Length; c++)
-    {
-        var currC = "";
-        for (int r = 0; r < gridR.Count; r++)
-        {
-            currC = $"{currC}{gridR[r][c]}";
-        }
-        gridC.Add(currC.ToArray());
-    }
-    byColumn.Add(gridC);
-}
-
-
-
-long part1 = 0;
-long part2 = 0;
-for (int i = 0; i < byColumn.Count; i++)
-{
-    var hR1 = findReflections(byRow[i]);
-    var vR1 = findReflections(byColumn[i]);
-
-    var originalR = hR1.Select(i => (Reflection.Horizontal, i)).Concat(vR1.Select(i => (Reflection.Vertical, i))).ToList();
-
-    part1 += 100 * hR1.Sum() + vR1.Sum();
-
-    //try flipping every bit
-    for (int r = 0; r < byRow[i].Count; r++)
-    {
-        for (int c = 0; c < byRow[i][0].Length; c++)
-        {
-            byRow[i][r][c] = byRow[i][r][c] == '.' ? '#' : '.';
-            byColumn[i][c][r] = byColumn[i][c][r] == '.' ? '#' : '.';
-
-            var hR2 = findReflections(byRow[i]);
-            var vR2 = findReflections(byColumn[i]);
-
-            var newR = hR2.Select(i => (Reflection.Horizontal, i)).Concat(vR2.Select(i => (Reflection.Vertical, i)))
-                .Except(originalR).ToList();
-
-            if (newR.Count == 1)
-            {
-                part2 += 100 * newR.Where(tp => tp.Item1 == Reflection.Horizontal).Sum(tp => tp.i) 
-                    + newR.Where(tp => tp.Item1 == Reflection.Vertical).Sum(tp => tp.i);
-                goto next;          
-            }
-
-            byRow[i][r][c] = byRow[i][r][c] == '.' ? '#' : '.';
-            byColumn[i][c][r] = byColumn[i][c][r] == '.' ? '#' : '.';
-        }
-    }
-next:;
-}
+    return (100 * hR0.Sum() + vR0.Sum(), 100 * hR1.Sum() + vR1.Sum());
+}).Aggregate((tp1, tp2) => (tp1.Item1 + tp2.Item1, tp1.Item2 + tp2.Item2));
 
 Console.WriteLine($"Part1: {part1}");
 Console.WriteLine($"Part2: {part2}");
 Console.WriteLine($"Elapsed time: {watch.ElapsedMilliseconds}ms");
 
-List<int> findReflections (List<char[]> grid) => Enumerable.Range(1, grid.Count - 1)
-        .Where(r => grid.Take(r).Reverse().Zip(grid.Skip(r))
-        .All(tp => tp.First.SequenceEqual(tp.Second))).ToList();
+List<(int r, int)> findReflectionSmudges (string[] grid) => 
+    Enumerable.Range(1, grid.Length - 1)
+        .Select(r => (r, grid.Take(r).Reverse().Zip(grid.Skip(r))
+            .Sum(tp => tp.First.Zip(tp.Second).Count(tp => tp.First != tp.Second)))).ToList();
 
-void printGrid(List<char[]> grid)
-{
-    foreach (var line in grid)
-    {
-        Console.WriteLine(String.Join("",line));
-    }
-    Console.WriteLine();
-}
-
-enum Reflection
-{
-    Horizontal,
-    Vertical
-}
+void printGrid(string[] grid) => grid.Concat(new[] { "" }).ToList().ForEach(Console.WriteLine);
