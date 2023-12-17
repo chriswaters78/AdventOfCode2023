c52f2975a07c2a24162cf6689fd4b5479ed94e7e
Christian <cwaters@truenorthit.co.uk>
2023-12-17 15:30:18 +0000

    refactored

--- 2023_16/Program.cs
+++ 2023_16/Program.cs
@@ -1,45 +1,21 @@
 ﻿var grid = File.ReadAllLines("input.txt");
-
 (int R, int C) = (grid.Length, grid.First().Length);
 
-{
-    var beams = new Queue<Beam>();
-    beams.Enqueue(new Beam(new Point(0, 0), Direction.East));
-    var energised = solve(beams);
+var part1 = new[] { new Beam(new Point(0, 0), Direction.East), new Beam(new Point(0, 0), Direction.South) }.Max(solve);
+Console.WriteLine($"Part1: {part1}");
 
-    //Console.WriteLine(printEnergised(energised));
-    var part1 = energised.Where(kvp => kvp.Value != 0).Count();
-    Console.WriteLine($"Part1: {part1}");
-}
-
-var part2 = -1;
-for (int r = 0; r < R; r++)
-{
-    var beams = new Queue<Beam>();
-    beams.Enqueue(new Beam(new Point(r, 0), Direction.East));
-    var energised = solve(beams);
-    part2 = Math.Max(part2, energised.Where(kvp => kvp.Value != 0).Count());
-    
-    beams.Enqueue(new Beam(new Point(r, C - 1), Direction.West));
-    energised = solve(beams);
-    part2 = Math.Max(part2, energised.Where(kvp => kvp.Value != 0).Count());
-}
-for (int c = 0; c < C; c++)
-{
-    var beams = new Queue<Beam>();
-    beams.Enqueue(new Beam(new Point(0, c), Direction.South));
-    var energised = solve(beams);
-    part2 = Math.Max(part2, energised.Where(kvp => kvp.Value != 0).Count());
-
-    beams.Enqueue(new Beam(new Point(R - 1, c), Direction.North));
-    energised = solve(beams);
-    part2 = Math.Max(part2, energised.Where(kvp => kvp.Value != 0).Count());
-}
+var part2 = Enumerable.Range(0, R)
+    .SelectMany(r => new[] { new Beam(new Point(r, 0), Direction.East), new Beam(new Point(r, C - 1), Direction.West) })
+    .Concat(Enumerable.Range(0, C).SelectMany(c => new[] { new Beam(new Point(0, c), Direction.South), new Beam(new Point(R - 1, c), Direction.North) }))
+    .Max(solve);
 
 Console.WriteLine($"Part2: {part2}");
 
-Dictionary<Point, int> solve(Queue<Beam> beams)
+int solve(Beam initialBeam)
 {
+    var beams = new Queue<Beam>();
+    beams.Enqueue(initialBeam);
+
     var energised = Enumerable.Range(0, R).SelectMany(r => Enumerable.Range(0, C).Select(c => new Point(r, c))).ToDictionary(p => p, _ => 0);
 
     while (beams.Any())
@@ -49,11 +25,14 @@ Dictionary<Point, int> solve(Queue<Beam> beams)
         if (beam.point.r < 0 || beam.point.c < 0 || beam.point.r >= R || beam.point.c >= C)
             continue;
 
+        //if we have already had a beam pass through here in the same direction, we can stop
         if ((energised[beam.point] & (int)beam.dir) > 0)
             continue;
 
+        //mark that we have passed through this point in this direction
         energised[beam.point] = energised[beam.point] | (int)beam.dir;
 
+        //add the any resulting beams to our queue
         switch ((beam.dir, grid[beam.point.r][beam.point.c]))
         {
             case (_, '.'):
@@ -64,28 +43,20 @@ Dictionary<Point, int> solve(Queue<Beam> beams)
                 beams.Enqueue(next(beam.point, beam.dir));
                 break;
             case (Direction.North, '/'):
-                beams.Enqueue(next(beam.point, Direction.East));
-                break;
-            case (Direction.East, '/'):
-                beams.Enqueue(next(beam.point, Direction.North));
-                break;
-            case (Direction.South, '/'):
-                beams.Enqueue(next(beam.point, Direction.West));
-                break;
-            case (Direction.West, '/'):
-                beams.Enqueue(next(beam.point, Direction.South));
-                break;
-            case (Direction.North, '\\'):
-                beams.Enqueue(next(beam.point, Direction.West));
-                break;
-            case (Direction.East, '\\'):
-                beams.Enqueue(next(beam.point, Direction.South));
-                break;
             case (Direction.South, '\\'):
                 beams.Enqueue(next(beam.point, Direction.East));
                 break;
+            case (Direction.East, '/'):
             case (Direction.West, '\\'):
                 beams.Enqueue(next(beam.point, Direction.North));
+                break;
+            case (Direction.South, '/'):
+            case (Direction.North, '\\'):
+                beams.Enqueue(next(beam.point, Direction.West));
+                break;
+            case (Direction.West, '/'):
+            case (Direction.East, '\\'):
+                beams.Enqueue(next(beam.point, Direction.South));
                 break;
             case (Direction.North, '-'):
             case (Direction.South, '-'):
@@ -100,7 +71,7 @@ Dictionary<Point, int> solve(Queue<Beam> beams)
         }
     }
 
-    return energised;
+    return energised.Where(kvp => kvp.Value != 0).Count();
 }
 
 string printEnergised(Dictionary<Point, int> energised)
@@ -110,34 +81,18 @@ string printEnergised(Dictionary<Point, int> energised)
     {
         for (int c = 0; c < C; c++)
         {
-            var point = new Point(r, c);
-            var directions = new List<Direction>();
-            if ((energised[point] & (int)Direction.North) != 0)
-                directions.Add(Direction.North);
-            if ((energised[point] & (int)Direction.East) != 0)
-                directions.Add(Direction.East);
-            if ((energised[point] & (int)Direction.South) != 0)
-                directions.Add(Direction.South);
-            if ((energised[point] & (int)Direction.West) != 0)
-                directions.Add(Direction.West);
-
-            if (directions.Count == 0)
+            var directions = Enumerable.Range(0, 4).Where(dir => (energised[new Point(r, c)] & dir) != 0).ToList();
+            sb.Append(directions switch
             {
-                sb.Append('.');
-            }
-            else if (directions.Count == 1)
-            {
-                sb.Append(directions.First() switch {
-                    Direction.North => '^',
-                    Direction.South => 'v',
-                    Direction.East => '>',
-                    Direction.West => '<',
-                });
-            }
-            else
-            {
-                sb.Append(directions.Count);
-            }
+                [] => '.',
+                [var dir] => dir switch {
+                    (int)Direction.North => '^',
+                    (int)Direction.East => 'v',
+                    (int)Direction.South => '>',
+                    (int)Direction.West => '<',
+                },
+                _ => directions.Count
+            });
         }
         sb.AppendLine();
     }
@@ -151,8 +106,8 @@ Beam next(Point point, Direction dir) => dir switch
     Direction.South => new Beam(new Point(point.r + 1, point.c), dir),
     Direction.West => new Beam(new Point(point.r, point.c - 1), dir),
 };
-record struct Beam(Point point, Direction dir);
 
+record struct Beam(Point point, Direction dir);
 record struct Point(int r, int c);
 
 [Flags]
