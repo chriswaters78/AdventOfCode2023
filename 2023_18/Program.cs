f60520dc73d1cf075cac1df89b9848b8921034a0
Christian Waters <cwaters@truenorthit.co.uk>
2023-12-18 19:16:55 +0000

    refactored and made Pick's applicability clearer

--- 2023_18/Program.cs
+++ 2023_18/Program.cs
@@ -1,46 +1,46 @@
-﻿using System.Collections;
-using System.Numerics;
-using System.Text;
+﻿using System.Numerics;
 
-var lines = File.ReadAllLines("input.txt").Select(line => line.Split(" ")).Select(arr => (arr[0..2], arr[2].Trim('(', ')', '#'))).Select(
-    tp => new { Dir1 = tp.Item1[0][0], Move1 = int.Parse(tp.Item1[1]), Dir2 = "RDLU"[int.Parse($"{tp.Item2.Last()}")], Move2 = int.Parse(tp.Item2[0..^1], System.Globalization.NumberStyles.HexNumber) }).ToList();
+var moves = new [] {
+        ( "R0", new Complex(0, 1) ),
+        ( "D1", new Complex(1, 0) ),
+        ( "L2", new Complex(0, -1) ),
+        ( "U3", new Complex(-1, 0) ) }
+    .SelectMany(tp => tp.Item1.Select(ch => (ch, tp.Item2)))
+    .ToDictionary(tp => tp.ch, tp => tp.Item2);
 
-var grid = new Dictionary<Complex, char>();
+var lines = File.ReadAllLines("input.txt").Select(line => line.Split(" ")).Select(arr => (arr[0..2], arr[2].Trim('(', ')', '#')));
+var part1 = lines.Select(tp => (moves[tp.Item1[0][0]], int.Parse(tp.Item1[1])));
+var part2 = lines.Select(tp => (moves[tp.Item2.Last()], int.Parse(tp.Item2[0..^1], System.Globalization.NumberStyles.HexNumber)));
 
-var part1 = solve(lines.Select(a => (a.Dir1, a.Move1)));
-Console.WriteLine($"Part1: {part1}");
-var part2 = solve(lines.Select(a => (a.Dir2, a.Move2)));
-Console.WriteLine($"Part2: {part2}");
+Console.WriteLine($"Part1: {solve(part1)}");
+Console.WriteLine($"Part2: {solve(part2)}");
 
-long solve(IEnumerable<(char d, int m)> steps)
+long solve(IEnumerable<(Complex d, int m)> steps)
 {
     var current = new Complex(0, 0);
     var vertices = new List<Complex>(new[] { current });
     foreach (var step in steps)
     {
-        current = current + step.m * move(step.d);
+        current = current + step.m * step.d;
         vertices.Add(current);
     }
 
-    var answer = 0L;
-    //run the shoelace algorithm
-    for (int v = vertices.Count - 1 ; v > 0; v--)
+    var A = 0L;
+    //Shoelace algorithm, A = sum over all anti-clockwise vertices (y+1 + y)(x+1 - x)/2
+    for (int v = 0; v <  vertices.Count - 1; v++)
     {
-        answer += ((long)vertices[v - 1].Real + (long)vertices[v].Real) * ((long)vertices[v - 1].Imaginary - (long)vertices[v].Imaginary);
+        A += ((long)vertices[v + 1].Real + (long)vertices[v].Real) * ((long)vertices[v + 1].Imaginary - (long)vertices[v].Imaginary);
     }
-    answer /= 2;
+    //running clockwise gives -'ve area
+    A /= -2;
 
-    //add the additional area within the border
-    //we have half the path length, plus one for the 4 excess right turns
-    answer += steps.Sum(tp => tp.m) / 2 + 1;
-
-    return answer;
+    //Pick's: A = i + b/2 - 1
+    //where A is area within a boundary running through midpoint of each # symbol
+    //we are effectively using it on a half integer grid which works fine
+    //i calculated from Pick's is then every interior point apart from the boundary
+    //so then just add the length of the boundary as well
+    var b = steps.Sum(tp => tp.m);
+    var i = A - b / 2 + 1;
+    return i + b;    
 }
 
-Complex move(char dir) => dir switch
-{
-    'R' => new Complex(0, 1),
-    'D' => new Complex(1, 0),
-    'L' => new Complex(0, -1),
-    'U' => new Complex(-1, 0),
-};
