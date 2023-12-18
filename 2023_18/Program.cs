d04efe7ecb30dd334fd1e4615d3ddefea69128de
Christian Waters <cwaters@truenorthit.co.uk>
2023-12-18 13:49:59 +0000

    improved by subtracting off path area, removed part 1 flood fill

--- 2023_18/Program.cs
+++ 2023_18/Program.cs
@@ -2,116 +2,40 @@
 using System.Numerics;
 using System.Text;
 
-var lines = File.ReadAllLines("input.txt").Select(line => line.Split(" ")).Select(arr => new { Dir = arr[0][0], Move = int.Parse(arr[1]), Colour = arr[2].Trim('(', ')', '#') }).ToList();
+var lines = File.ReadAllLines("input.txt").Select(line => line.Split(" ")).Select(arr => (arr[0..2], arr[2].Trim('(', ')', '#'))).Select(
+    tp => new { Dir1 = tp.Item1[0][0], Move1 = int.Parse(tp.Item1[1]), Dir2 = "RDLU"[int.Parse($"{tp.Item2.Last()}")], Move2 = int.Parse(tp.Item2[0..^1], System.Globalization.NumberStyles.HexNumber) }).ToList();
 
 var grid = new Dictionary<Complex, char>();
 
-part1();
-part2();
-void part2()
+var part1 = solve(lines.Select(a => (a.Dir1, a.Move1)));
+Console.WriteLine($"Part1: {part1}");
+var part2 = solve(lines.Select(a => (a.Dir2, a.Move2)));
+Console.WriteLine($"Part2: {part2}");
+
+long solve(IEnumerable<(char d, int m)> steps)
 {
     var current = new Complex(0, 0);
     var vertices = new List<Complex>(new[] { current });
-    long pathLength = 0;
-    foreach ((var line, int li) in lines.Select((str, i) => (str, i)))
+    foreach (var step in steps)
     {
-        long num = Int32.Parse(line.Colour[0..^1], System.Globalization.NumberStyles.HexNumber);
-        var dir = hexToDir(line.Colour);
-        var prevDir = hexToDir(lines[(li + lines.Count - 1) % lines.Count].Colour);
-        var nextDir = hexToDir(lines[(li + 1) % lines.Count].Colour);
-
-        current = current + (num + 1 - (turnR(prevDir, dir) ? 0 : 1) - (turnR(dir, nextDir) ? 0 : 1)) * move(dir);
-        pathLength += num;
+        current = current + step.m * move(step.d);
         vertices.Add(current);
     }
 
-    vertices.Reverse();
-
-    var part2 = 0L;
-
+    var answer = 0L;
     //run the shoelace algorithm
-    for (int v = 0; v < vertices.Count - 1; v++)
+    for (int v = vertices.Count - 1 ; v > 0; v--)
     {
-        var ys = (long)vertices[v + 1].Real + (long)vertices[v].Real;
-        var xs = (long)vertices[v + 1].Imaginary - (long)vertices[v].Imaginary;
-        part2 += xs * ys;
+        answer += ((long)vertices[v - 1].Real + (long)vertices[v].Real) * ((long)vertices[v - 1].Imaginary - (long)vertices[v].Imaginary);
     }
+    answer /= 2;
 
-    part2 /= 2;
+    //add the additional area within the border
+    //we have half the path length, plus one for the 4 excess right turns
+    answer += steps.Sum(tp => tp.m) / 2 + 1;
 
-    Console.WriteLine($"Part2: {part2}");
+    return answer;
 }
-
-
-void part1()
-{
-    var current = new Complex(0, 0);
-    foreach (var line in lines)
-    {
-        for (int m = 0; m < line.Move; m++)
-        {
-            grid[current] = '#';
-            current = current + move(line.Dir);
-        }
-    }
-    (var R0, var R1) = (grid.Keys.Min(key => (int)key.Real) - 1, grid.Keys.Max(key => (int)key.Real) + 1);
-    (var C0, var C1) = (grid.Keys.Min(key => (int)key.Imaginary) - 1, grid.Keys.Max(key => (int)key.Imaginary) + 1);
-
-    Console.WriteLine(print(grid));
-
-    var queue = new Queue<Complex>();
-    queue.Enqueue(new Complex(R0, C0));
-    while (queue.Any())
-    {
-        var curr = queue.Dequeue();
-
-        foreach (var mv in new[] { move('R'), move('L'), move('D'), move('U') })
-        {
-            var next = curr + mv;
-            if (next.Real < R0 || next.Real > R1 || next.Imaginary < C0 || next.Imaginary > C1)
-            {
-                continue;
-            }
-            if (grid.ContainsKey(next))
-            {
-                continue;
-            }
-            grid[next] = 'O';
-
-            queue.Enqueue(next);
-        }
-    }
-
-
-    Console.WriteLine(print(grid));
-
-    var part1 = Enumerable.Range(R0, R1 - R0).SelectMany(r => Enumerable.Range(C0, C1 - C0).Select(c =>
-    {
-        var key = new Complex(r, c);
-        if (!grid.ContainsKey(key) || grid[key] == '#')
-            return 1;
-        return 0;
-    })).Sum();
-
-    Console.WriteLine($"Part1: {part1}");
-
-    string print(Dictionary<Complex, char> grid)
-    {
-        StringBuilder sb = new StringBuilder();
-        for (int r = R0; r <= R1; r++)
-        {
-            for (int c = C0; c <= C1; c++)
-            {
-                var key = new Complex(r, c);
-                sb.Append(grid.ContainsKey(key) ? grid[key] : '.');
-            }
-            sb.AppendLine();
-        }
-
-        return sb.ToString();
-    }
-}
-
 
 Complex move(char dir) => dir switch
 {
@@ -120,23 +44,3 @@ Complex move(char dir) => dir switch
     'L' => new Complex(0, -1),
     'U' => new Complex(-1, 0),
 };
-
-bool turnR(char from, char to) => (from, to) switch
-{
-    ('R', 'D') => true,
-    ('R', 'U') => false,
-    ('D', 'L') => true,
-    ('D', 'R') => false,
-    ('L', 'U') => true,
-    ('L', 'D') => false,
-    ('U', 'R') => true,
-    ('U', 'L') => false,
-};
-
-char hexToDir(string hex) => hex.Last() switch
-    {
-        '0' => 'R',
-        '1' => 'D',
-        '2' => 'L',
-        '3' => 'U',
-    };
