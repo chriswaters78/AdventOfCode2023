96795f74f57b10a522c9319451907d39d3986d0d
Christian <cwaters@truenorthit.co.uk>
2023-12-20 23:25:39 +0000

    refactored

--- 2023_20/Program.cs
+++ 2023_20/Program.cs
@@ -1,127 +1,73 @@
 ﻿using System.Numerics;
 
-var stateFlipFlop = new Dictionary<string, bool>();
-var stateConjunction = new Dictionary<string, Dictionary<string,bool>>();
-
 var modules = File.ReadAllLines("input.txt").Select(line => {
     var sp = line.Split(" -> ");
-
-    Type type;
-    string name;
     var outputs = sp[1].Split(", ").ToList();
     switch (sp[0])
     {
         case string flip when flip.StartsWith("%"):
-            type = Type.Flipflop;
-            name = sp[0][1..];
-            stateFlipFlop.Add(name, false);
-            break;
+            return new Module(Type.Flipflop, sp[0][1..], outputs);
         case string conj when conj.StartsWith("&"):
-            type = Type.Conjunction;
-            name = sp[0][1..];
-            stateConjunction[name] = new Dictionary<string, bool>();
-            break;
+            return new Module(Type.Conjunction, sp[0][1..], outputs);
         default:
-            type = Type.Broadcaster;
-            name = "broadcaster";
-            break;
+            return new Module(Type.Broadcaster, "broadcaster", outputs);
     };
-
-    return new Module(type, name, outputs);
 }).ToDictionary(md => md.name, md => md);
 
-foreach (var conj in stateConjunction.Keys)
-{
-    //find all modules that connect to this
-    foreach (var input in modules.Where(kvp => kvp.Value.outputs.Contains(conj)))
-    {
-        stateConjunction[conj].Add(input.Key, false);
-    }
-}
-
-var signals = new Queue<Signal>();
-long part1L = 0;
-long part1H = 0;
-long part1 = 0;
-BigInteger part2 = 0;
+var states = modules.Keys.ToDictionary(name => name, _ => false);
+var inputs = modules.Values.ToDictionary(m => m.name, m => modules.Values.Where(input => input.outputs.Any(output => m.name == output)).Select(input => input.name).ToList());
 
 var finalConjunctions = new[] { "db", "ln", "vq", "tf" };
 var finalConjunctionsFiredAt = finalConjunctions.ToDictionary(str => str, _ => -1L);
 
-for (long press = 1; press <= long.MaxValue; press++)
+(long part1L, long part1H, BigInteger part2) = (0, 0, 0);
+for (long press = 1; press <= long.MaxValue; press++) 
 {
-    if (press == 1001)
-    {
-        part1 = part1H * part1L;
-    }
+    if (press == 1001) Console.WriteLine($"Part1: {part1H * part1L}");
+
     if (finalConjunctionsFiredAt.All(kvp => kvp.Value != -1))
     {
-        var numerator = finalConjunctionsFiredAt.Values.Aggregate(1L, (acc, i) => acc * i);
-        var denominator = finalConjunctionsFiredAt.Values.Select(i => new BigInteger(i)).Aggregate(BigInteger.GreatestCommonDivisor);
-        part2 = numerator / denominator;
-        goto done;
+        //apparently they are always primes for the puzzle inputs
+        part2 = finalConjunctionsFiredAt.Values.Aggregate((i1,i2) => i1 * i2)
+            / finalConjunctionsFiredAt.Values.Select(i => (BigInteger) i).Aggregate(BigInteger.GreatestCommonDivisor);
+        
+        Console.WriteLine($"Part2: {part2}");
+        return;
     }
 
-    part1L++;
-    signals.Enqueue(new Signal("button", "broadcaster", false));
-    while (signals.Count > 0)
+    var queue = new Queue<Signal>( new[] { new Signal("broadcaster", false) });
+    while (queue.Count > 0)
     {
-        var curr = signals.Dequeue();
-        if (!modules.ContainsKey(curr.to))
-        {
+        var current = queue.Dequeue();
+        
+        if (current.signal) part1H++; else part1L++;
+        if (!current.signal && finalConjunctionsFiredAt.TryGetValue(current.to, out long lastFired) && lastFired == -1)
+            finalConjunctionsFiredAt[current.to] = press;
+
+        if (!modules.ContainsKey(current.to))
             continue;
-        }
-        var targetModule = modules[curr.to];
-        switch (targetModule.type)
+
+        var module = modules[current.to];      
+        if (module.type == Type.Flipflop && current.signal)
+            continue;
+
+        var signalOutput = module.type switch
         {
-            case Type.Broadcaster:
-                foreach (var target in targetModule.outputs)
-                {
-                    if (curr.signal)
-                        part1H++;
-                    else
-                        part1L++;
-                    signals.Enqueue(new Signal(targetModule.name, target, curr.signal));
-                }
-                break;
-            case Type.Conjunction:
-                stateConjunction[targetModule.name][curr.from] = curr.signal;
-                var output = !stateConjunction[targetModule.name].Values.All(b => b);
-                foreach (var target in targetModule.outputs)
-                {
-                    if (!output && finalConjunctions.Contains(target) && finalConjunctionsFiredAt[target] == -1)
-                    {
-                        finalConjunctionsFiredAt[target] = press;
-                    }
-                    if (output)
-                        part1H++;
-                    else
-                        part1L++;
-                    signals.Enqueue(new Signal(targetModule.name, target, output));
-                }
-                break;
-            case Type.Flipflop:
-                if (curr.signal)
-                    continue;
-                stateFlipFlop[targetModule.name] = !stateFlipFlop[targetModule.name];
-                foreach (var target in targetModule.outputs)
-                {
-                    if (stateFlipFlop[targetModule.name])
-                        part1H++;
-                    else
-                        part1L++;
-                    signals.Enqueue(new Signal(targetModule.name, target, stateFlipFlop[targetModule.name]));
-                }
-                break;
+            Type.Broadcaster => false,
+            Type.Conjunction => !inputs[module.name].All(input => states[input]),
+            Type.Flipflop => !states[module.name],
+        };
+
+        foreach (var output in module.outputs)
+        {
+            states[module.name] = signalOutput;
+            queue.Enqueue(new Signal(output, signalOutput));
         }
     }
 }
-done:;
-Console.WriteLine($"Part1: {part1}");
-Console.WriteLine($"Part2: {part2}");
 
 record struct Module(Type type, string name, List<string> outputs);
-record struct Signal(string from, string to, bool signal);
+record struct Signal(string to, bool signal);
 enum Type
 {
     Broadcaster,
