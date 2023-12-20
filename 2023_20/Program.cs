ae1525d10afae1d8384cd0a0b2c1fef635fc1980
Christian <cwaters@truenorthit.co.uk>
2023-12-20 23:28:53 +0000

    .

--- 2023_20/Program.cs
+++ 2023_20/Program.cs
@@ -17,18 +17,18 @@ var modules = File.ReadAllLines("input.txt").Select(line => {
 var states = modules.Keys.ToDictionary(name => name, _ => false);
 var inputs = modules.Values.ToDictionary(m => m.name, m => modules.Values.Where(input => input.outputs.Any(output => m.name == output)).Select(input => input.name).ToList());
 
-var finalConjunctionsFiredAt = new[] { "db", "ln", "vq", "tf" }.ToDictionary(str => str, _ => -1L);
+var finalConjunctions = new[] { "db", "ln", "vq", "tf" }.ToDictionary(str => str, _ => -1L);
 
 (long part1L, long part1H, BigInteger part2) = (0, 0, 0);
 for (long press = 1; press <= long.MaxValue; press++) 
 {
     if (press == 1001) Console.WriteLine($"Part1: {part1H * part1L}");
 
-    if (finalConjunctionsFiredAt.All(kvp => kvp.Value != -1))
+    if (finalConjunctions.All(kvp => kvp.Value != -1))
     {
         //apparently they are always primes for the puzzle inputs
-        part2 = finalConjunctionsFiredAt.Values.Aggregate((i1,i2) => i1 * i2)
-            / finalConjunctionsFiredAt.Values.Select(i => (BigInteger) i).Aggregate(BigInteger.GreatestCommonDivisor);
+        part2 = finalConjunctions.Values.Aggregate((i1,i2) => i1 * i2)
+            / finalConjunctions.Values.Select(i => (BigInteger) i).Aggregate(BigInteger.GreatestCommonDivisor);
         
         Console.WriteLine($"Part2: {part2}");
         return;
@@ -40,8 +40,8 @@ for (long press = 1; press <= long.MaxValue; press++)
         var current = queue.Dequeue();
         
         if (current.signal) part1H++; else part1L++;
-        if (!current.signal && finalConjunctionsFiredAt.TryGetValue(current.to, out long lastFired) && lastFired == -1)
-            finalConjunctionsFiredAt[current.to] = press;
+        if (!current.signal && finalConjunctions.TryGetValue(current.to, out long lastFired) && lastFired == -1)
+            finalConjunctions[current.to] = press;
 
         if (!modules.ContainsKey(current.to))
             continue;
