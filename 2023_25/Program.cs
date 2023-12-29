e782e10543a86e2681629f9544dc337704983263
Christian Waters <cwaters@truenorthit.co.uk>
2023-12-29 16:49:18 +0000

    stoer-wagner

--- 2023_25/Program.cs
+++ 2023_25/Program.cs
@@ -1,7 +1,22 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
 double Sqrt2 = Math.Sqrt(2);
 Random rand = new Random();
-var stopwatch = Stopwatch.StartNew();
+var stopwatch = new Stopwatch();
+
+//this is the example graph from their paper
+//https://citeseerx.ist.psu.edu/document?repid=rep1&type=pdf&doi=b10145f7fc3d07e43607abc2a148e58d24ced543
+//var testGraph = new Dictionary<string, Dictionary<string, int>>();
+//testGraph["one"] = new Dictionary<string, int>() { { "two", 2 }, { "five", 3 } };
+//testGraph["two"] = new Dictionary<string, int>() { { "one", 2 }, { "five", 2 }, { "six", 2 }, { "three", 3 } };
+//testGraph["three"] = new Dictionary<string, int>() { { "two", 3 }, { "four", 4 }, { "seven", 2 } };
+//testGraph["four"] = new Dictionary<string, int>() { { "three", 4 }, { "seven", 2 }, { "eight", 2 } };
+//testGraph["five"] = new Dictionary<string, int>() { { "one", 3 }, { "two", 2 }, { "six", 3 } };
+//testGraph["six"] = new Dictionary<string, int>() { { "five", 3 }, { "two", 2 }, { "seven", 1 } };
+//testGraph["seven"] = new Dictionary<string, int>() { { "six", 1 }, { "three", 2 }, { "four", 2 }, { "eight", 3 } };
+//testGraph["eight"] = new Dictionary<string, int>() { { "seven", 3 }, { "four", 2 } };
+//minimumCutStoerWagner(testGraph, "two");
+
 
 var graph = new Dictionary<string, List<string>>();
 foreach (var line in File.ReadAllLines("input.txt"))
@@ -21,42 +36,128 @@ foreach (var line in File.ReadAllLines("input.txt"))
 
 Console.WriteLine($"{graph.Keys.Count} nodes, edges {graph.Values.Sum(list => list.Count)}");
 
-kragersSimple(graph);
+HashSet<string> prevSet1 = null;
+HashSet<string> prevSet2 = null;
+for (int i = 0; i < 100; i++)
+{
+    foreach (var (name, resultFactory) in new (string name, Func<(int minCut, string partition)>)[] {
+        ("Stoer-Wagner", () => minimumCutStoerWagner(graph)),
+        ("Kragers (recursive)", () => kragers(graph, 3, true)),
+        ("Kragers (non-recursive)", () => kragers(graph, 3, false))})
+    {
+        stopwatch.Restart();
+        (var minCut, string partition) = resultFactory();
+        var set1 = partition.Select((ch, i) => (ch, i)).GroupBy(tp => tp.i / 3).Select(grp => new String(grp.OrderBy(tp => tp.i).Select(tp => tp.ch).ToArray())).ToHashSet();
+        var set2 = graph.Keys.Where(key => !set1.Contains(key)).ToHashSet();
 
-void kragersSimple(Dictionary<string, List<string>> grap)
+
+        Console.WriteLine($"{name} found min cut of {minCut}, between a partition of size {set1.Count} and {set2.Count} in {stopwatch.ElapsedMilliseconds} ms");
+        Console.WriteLine($"{name} Part2: {set1.Count * set2.Count}");
+
+        (set1, set2) = set1.Count <= set2.Count ? (set1, set2) : (set2, set1);
+        if (prevSet1 != null)
+        {
+            if (!prevSet1.OrderBy(str => str).SequenceEqual(set1.OrderBy(str => str)))
+            {
+                Console.WriteLine("Set did not match previous found set");
+            }
+            else
+            {
+                Console.WriteLine("Sets matched previous minCut found");
+            }
+        }
+        (prevSet1, prevSet2) = (set1, set2);
+    }
+}
+
+(int minCut, string partition) minimumCutStoerWagner(Dictionary<string, List<string>> graph)
+{
+    var a = graph.Keys.ToArray()[rand.Next(graph.Count)];
+    var minCut = int.MaxValue;
+    string minPartition = null;
+
+    var g = graph.ToDictionary(kvp => kvp.Key, kvp => kvp.Value.ToDictionary(str => str, _ => 1));
+    while (g.Count > 1)
+    {
+        (var cutOfPhase, var partition) = minimumCutPhaseStoerWagner(g, a);
+        if (cutOfPhase < minCut)
+        {
+            minCut = cutOfPhase;
+            minPartition = partition;
+        }
+    }
+
+    return (minCut, minPartition);
+}
+(int minCut, string partition) minimumCutPhaseStoerWagner(Dictionary<string, Dictionary<string,int>> graph, string a)
+{
+    var A = new List<string>();
+    A.Add(a);
+    var weights = new Dictionary<string, int>();
+    foreach (var v in graph.Keys)
+    {
+        if (v == a)
+            continue;
+
+        if (graph[v].ContainsKey(a))
+        {
+            weights[v] = graph[v][a];
+        }
+        else
+        {
+            weights[v] = 0;
+        }
+    }
+    int cutOfPhase = int.MaxValue;
+    while (A.Count != graph.Count)
+    {
+        //find the most strongly connected vertex to A
+        (var next, cutOfPhase) = weights.MaxBy(kvp => kvp.Value);
+        weights.Remove(next);
+        A.Add(next);
+        //update queue, every vertex connected to next must have its priority increased by the weight of edge to next
+        foreach ((var connected, var count) in graph[next])
+        {
+            if (weights.ContainsKey(connected))
+            {
+                weights[connected] = weights[connected] + count;
+            }
+        }
+    }
+
+    //now we must shrink G by merging the last two added nodes
+    graph = contractEdge(graph, A[A.Count - 2], A[A.Count - 1]);
+    return (cutOfPhase, A.Last());
+}
+(int, string) kragers(Dictionary<string, List<string>> grap, int FINDCUT, bool useRecursive)
 {
     int bestCut = int.MaxValue;
+    string bestPartition = null;
     stopwatch.Restart();
-    for (int n = 1; n < 1000000; n++)
+    while (bestCut != FINDCUT)
     {
-        var result = recursiveContract(graph.ToDictionary(kvp => kvp.Key, kvp => kvp.Value.ToDictionary(str => str, _ => 1)));
-        //var result = contract(graph.ToDictionary(kvp => kvp.Key, kvp => kvp.Value.ToDictionary(str => str, _ => 1)),2);
+        var result = useRecursive ? recursiveContractKragers(graph.ToDictionary(kvp => kvp.Key, kvp => kvp.Value.ToDictionary(str => str, _ => 1)))
+            : contractKragers(graph.ToDictionary(kvp => kvp.Key, kvp => kvp.Value.ToDictionary(str => str, _ => 1)), 2); ;
+        
         var minCut = result.Values.First().Sum(kvp => kvp.Value);
         if (minCut < bestCut)
         {
             bestCut = minCut;
-            Console.WriteLine($"Cut of {bestCut} found on iteration {n} in {stopwatch.ElapsedMilliseconds}");
+            bestPartition = result.Keys.First();
         }
-        if (minCut == 3)
-        {
-            Console.WriteLine($"Found min cut of 3 found between {result.Keys.First()} and {result.Keys.Skip(1).Single()}");
-            Console.WriteLine($"{result.Keys.First().Length * result.Keys.Skip(1).Single().Length / 9}");
-            break;
-        }
-        else
-        {
-            Console.WriteLine($"Iteration {n} found cut of {minCut}, average time per iteration {stopwatch.ElapsedMilliseconds / n}ms");
-        }
+        //Console.WriteLine($"Found min cut of 3 found between {result.Keys.First()} and {result.Keys.Skip(1).Single()}");
+        //Console.WriteLine($"{result.Keys.First().Length * result.Keys.Skip(1).Single().Length / 9}");
     }
+    return (bestCut, bestPartition);
 }
 
-Dictionary<string, Dictionary<string, int>> recursiveContract(Dictionary<string, Dictionary<string,int>> graph)
+Dictionary<string, Dictionary<string, int>> recursiveContractKragers(Dictionary<string, Dictionary<string,int>> graph)
 {
     int N = graph.Count;
     if (N < 6)
     {
         var g = graph.ToDictionary(KeyValuePair => KeyValuePair.Key, KeyValuePair => KeyValuePair.Value.ToDictionary(kvp2 => kvp2.Key, kvp2 => kvp2.Value));
-        g = contract(g, 2);
+        g = contractKragers(g, 2);
         return g;
     }
     else
@@ -64,29 +165,29 @@ Dictionary<string, Dictionary<string, int>> recursiveContract(Dictionary<string,
         var limit = (int)Math.Ceiling(N / (Sqrt2 + 1));
         
         var g1 = graph.ToDictionary(KeyValuePair => KeyValuePair.Key, KeyValuePair => KeyValuePair.Value.ToDictionary(kvp2 => kvp2.Key, kvp2 => kvp2.Value));
-        g1 = contract(g1, limit);
-        var r1 = recursiveContract(g1);
+        g1 = contractKragers(g1, limit);
+        var r1 = recursiveContractKragers(g1);
         
         var g2 = graph.ToDictionary(KeyValuePair => KeyValuePair.Key, KeyValuePair => KeyValuePair.Value.ToDictionary(kvp2 => kvp2.Key, kvp2 => kvp2.Value));
-        g2 = contract(g2, limit);
-        var r2 = recursiveContract(g2);
+        g2 = contractKragers(g2, limit);
+        var r2 = recursiveContractKragers(g2);
 
         return r1.Values.First().Sum(kvp => kvp.Value) < r2.Values.First().Sum(kvp => kvp.Value) ? r1 : r2;
     }
 }
 
-Dictionary<string, Dictionary<string, int>> contract(Dictionary<string, Dictionary<string, int>> graph, int k)
+Dictionary<string, Dictionary<string, int>> contractKragers(Dictionary<string, Dictionary<string, int>> graph, int k)
 {
     while (graph.Count > k)
     {
-        (var vertex, var edge) = randomSelect(graph);
+        (var vertex, var edge) = randomSelectKragers(graph);
         graph = contractEdge(graph, vertex, edge);
     }
 
     return graph;
 }
 
-(string vertex, string edge) randomSelect(Dictionary<string, Dictionary<string, int>> graph)
+(string vertex, string edge) randomSelectKragers(Dictionary<string, Dictionary<string, int>> graph)
 {
     //find weights for all keys
     var Ds = new List<(string key, int cumulativeWeight)>();
@@ -169,6 +270,9 @@ Dictionary<string, Dictionary<string, int>> contractEdge(Dictionary<string, Dict
     return graph;
 }
 
+
+//original hacked up version
+//should work but seems inconsistent?
 //void contract(int maxDepth)
 //{
 //    var rand = new Random();
